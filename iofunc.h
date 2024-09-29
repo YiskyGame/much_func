bb0bdbfc3a09e65a0798bfcebe996dab21b2bc2f
YiskyGame <mc-cuiu001@outlook.com>
2024-09-29 20:50:04 +0800

    Update iofunc.h

--- iofunc.h
+++ iofunc.h
@@ -30,28 +30,11 @@ namespace much {
 			input(a[i]);
 	}
 
-	// 10_2
-	template<typename T>
-	void input_some(std::vector<T>& a, const int len) {
-		for (int i = 0; i < len; i++) {
-			T sth = input(sth);
-			a.push_back(sth);
-		}
-	}
-
 	// 11_1
 	template<typename T>
 	void output_some(const T *a, const int len, const std::string Begin = "", const std::string end = " ", const std::string Lend = "\n") {
 		for (int i = 0; i < len; ++i)
 			output(a[i], Begin, end);
-		std::cout << Lend;
-	}
-
-	// 11_2
-	template<typename T>
-	void output_some(std::vector<T>& a, const std::string Begin = "", const std::string end = " ", const std::string Lend = "\n") {
-		for (T x : a)
-			output(x, Begin, end);
 		std::cout << Lend;
 	}
 
@@ -79,13 +62,6 @@ namespace much {
 			input(a[i]);
 	}
 
-	// 20_2
-	template<typename T>
-	void input_some_a_to_b(std::vector<T>& a, const int A, const int B) {
-		for (int i = A; i <= B; ++i)
-			input(a[i]);
-	}
-
 	// 21
 	template<typename T>
 	void output_some_a_to_b(const T *a, const int A, const int B, const std::string Begin = "", const std::string end = " ", const std::string Lend = "\n") {
@@ -94,16 +70,8 @@ namespace much {
 		std::cout << Lend;
 	}
 
-	// 21_2
-	template<typename T>
-	void output_some_a_to_b(std::vector<T>& a, const int A, const int B, const std::string Begin = "", const std::string end = " ", const std::string Lend = "\n") {
-		for (int i = A; i <= B; ++i)
-			output(a[i], Begin, end);
-		std::cout << Lend;
-	}
-
 	// 34
-	long long read(long long &m) {
+	long long fast_read(long long &m) {
 		long long f = 1, n = 0;
 		char c = getchar();
 		while (c < '0' || c >= '9') {
@@ -117,14 +85,14 @@ namespace much {
 		m = n * f;
 		return n * f;
 	}
-	
+
 	// 35
-	void write(long long n) {
+	void fast_write(long long n) {
 		if(n < 0) {
 			putchar('-');
 			n = -n;
 		}
-		if(n > 9) write(n / 10);
+		if(n > 9) fast_write(n / 10);
 		putchar(n % 10 + '0');
 	}
 }
